6a0586de9d43f972b00b2f6f10e8a5848ecdf904
Rubik1g <57530370+Rubik1g@users.noreply.github.com>
2023-01-13 11:31:25 +0100

    récupère les données du body en format json

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
@@ -12,8 +14,11 @@ use Symfony\Component\Security\Http\Attribute\CurrentUser;
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'app_api_login')]
-    public function index(#[CurrentUser] ?User $user): JsonResponse
+    public function index(Request $request, UserRepository $doctrine): JsonResponse
     {
+        $user = json_decode($request->getContent(), true);
+        var_dump($user);
+
         if (null === $user) {
             return $this->json([
                 'message' => 'missing credentials',
@@ -22,7 +27,7 @@ class ApiLoginController extends AbstractController
         $token = "000000000"; // somehow create an API token for $user
 
         return $this->json([
-            'user' => $user->getUserIdentifier(),
+            //'user' => $user->getUserIdentifier(),
             'token' => $token,
         ]);
     }
